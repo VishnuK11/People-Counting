0047249f57e2a8179270aa669e77d3f378fabbd8
Vishnu Praneeth Kapalavai <30376279+VishnuK11@users.noreply.github.com>
2021-03-29 00:24:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,84 @@
+# People-Counting Crowds Using Yolov4+DeepSort
+
+This project involves Object tracking implemented with YOLOv4, DeepSort, and TensorFlow. The code is customized to track people in crowds specifically, however, can be modified for other object categories. YOLOv4 is a state of the art algorithm that uses deep convolutional neural networks to perform object detections. Deep SORT is an object tracking module. We can take the output of YOLOv4 feed these object detections into Deep SORT in order to create a highly accurate object tracker.
+
+## Demo of Object Tracker on Persons
+<p align="center"><img src="data/helpers/demo.gif"\></p>
+
+## Demo of Object Tracker on Cars
+<p align="center"><img src="data/helpers/cars.gif"\></p>
+
+## Getting Started
+To get started, install the proper dependencies either via Anaconda or Pip. I recommend Anaconda route for people using a GPU as it configures CUDA toolkit version for you.
+
+### Pip
+(TensorFlow 2 packages require a pip version >19.0.)
+```bash
+# TensorFlow CPU
+pip install -r requirements.txt
+
+# TensorFlow GPU
+pip install -r requirements-gpu.txt
+```
+
+## Downloading Official YOLOv4 Pre-trained Weights
+Our object tracker uses YOLOv4 to make the object detections, which deep sort then uses to track. There exists an official pre-trained YOLOv4 object detector model that is able to detect 80 classes. For easy demo purposes we will use the pre-trained weights for our tracker.
+Download pre-trained yolov4.weights file: https://drive.google.com/open?id=1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT
+
+Copy and paste yolov4.weights from your downloads folder into the 'data' folder of this repository.
+
+If you want to use yolov4-tiny.weights, a smaller model that is faster at running detections but less accurate, download file here: https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights
+
+## Running the Tracker with YOLOv4
+To implement the object tracking using YOLOv4, first we convert the .weights into the corresponding TensorFlow model which will be saved to a checkpoints folder. Then all we need to do is run the object_tracker.py script to run our object tracker with YOLOv4, DeepSort and TensorFlow.
+
+## Running the Tracker with YOLOv4
+# Run yolov4 object tracker
+python object_tracker.py --weights ./checkpoints/yolov4-416 --model yolov4 --video ./data/video/test.mp4 --output ./outputs/outputs.avi 
+
+If running on collab, add ! prefix before python for the above line.
+```
+
+## Resulting Video
+As mentioned above, the resulting video will save to wherever you set the ``--output`` command line flag path to. I always set it to save to the 'outputs' folder. You can also change the type of video saved by adjusting the ``--output_format`` flag, by default it is set to AVI codec which is XVID.
+
+Example video showing tracking of all coco dataset classes:
+<p align="center"><img src="data/helpers/all_classes.gif"\></p>
+
+## Filter Classes that are Tracked by Object Tracker
+By default the code is setup to track only class - person. However, you can easily adjust a few lines of code in order to track more than 1 or combination of the 80 classes. To filter a custom selection of classes all you need to do is comment out line 159 and uncomment out line 162 of [object_tracker.py] The classes can be any of the 80 that the model is trained on, see which classes you can track in the file [data/classes/coco.names]
+
+## Command Line Args Reference
+  
+ object_tracker.py:
+  --video: path to input video (use 0 for webcam)
+    (default: './data/video/test.mp4')
+  --output: path to output video (remember to set right codec for given format. e.g. XVID for .avi)
+    (default: None)
+  --output_format: codec used in VideoWriter when saving video to file
+    (default: 'XVID)
+  --[no]tiny: yolov4 or yolov4-tiny
+    (default: 'false')
+  --weights: path to weights file
+    (default: './checkpoints/yolov4-416')
+  --framework: what framework to use (tf, trt, tflite)
+    (default: tf)
+  --model: yolov3 or yolov4
+    (default: yolov4)
+  --size: resize images to
+    (default: 416)
+  --iou: iou threshold
+    (default: 0.45)
+  --score: confidence threshold
+    (default: 0.50)
+  --dont_show: dont show video output
+    (default: False)
+  --info: print detailed info about tracked objects
+    (default: False)
+```
+
+### References  
+
+   Huge shoutout goes to hunglc007 and nwojke for creating the backbones of this repository:
+  * [tensorflow-yolov4-tflite](https://github.com/hunglc007/tensorflow-yolov4-tflite)
+  * [Deep SORT Repository](https://github.com/nwojke/deep_sort)
---
+++ deep_sort/tracker.py
@@ -0,0 +1,224 @@
+from __future__ import absolute_import
+import numpy as np
+from . import kalman_filter
+from . import linear_assignment
+from . import iou_matching
+from .track import Track
+from _collections import deque
+import matplotlib.pyplot as plt
+import cv2
+
+
+class Tracker:
+   """
+   This is the multi-target tracker.
+
+   Parameters
+   ----------
+   metric : nn_matching.NearestNeighborDistanceMetric
+       A distance metric for measurement-to-track association.
+   max_age : int
+       Maximum number of missed misses before a track is deleted.
+   n_init : int
+       Number of consecutive detections before the track is confirmed. The
+       track state is set to `Deleted` if a miss occurs within the first
+       `n_init` frames.
+
+   Attributes
+   ----------
+   metric : nn_matching.NearestNeighborDistanceMetric
+       The distance metric used for measurement to track association.
+   max_age : int
+       Maximum number of missed misses before a track is deleted.
+   n_init : int
+       Number of frames that a track remains in initialization phase.
+   kf : kalman_filter.KalmanFilter
+       A Kalman filter to filter target trajectories in image space.
+   tracks : List[Track]
+       The list of active tracks at the current time step.
+
+   """
+
+   def __init__(self, metric, max_iou_distance=0.7, max_age=60, n_init=3):
+       self.metric = metric
+       self.max_iou_distance = max_iou_distance
+       self.max_age = max_age
+       self.n_init = n_init
+
+       self.kf = kalman_filter.KalmanFilter()
+       self.tracks = []
+       self._next_id = 1
+       self.center = [deque(maxlen=30) for _ in range(1000)]
+       self.cum_disp = [0 for _ in range(1000)]
+       self.unique_count = 0
+       self.current_count = 0
+       self.counter = []
+
+   def predict(self):
+       """Propagate track state distributions one time step forward.
+
+       This function should be called once every time step, before `update`.
+       """
+       for track in self.tracks:
+           track.predict(self.kf)
+
+   def update(self, detections):
+       """Perform measurement update and track management.
+
+       Parameters
+       ----------
+       detections : List[deep_sort.detection.Detection]
+           A list of detections at the current time step.
+
+       """
+       # Run matching cascade.
+       matches, unmatched_tracks, unmatched_detections = \
+           self._match(detections)
+
+       # Update track set.
+       for track_idx, detection_idx in matches:
+           self.tracks[track_idx].update(
+               self.kf, detections[detection_idx])
+       for track_idx in unmatched_tracks:
+           self.tracks[track_idx].mark_missed()
+       for detection_idx in unmatched_detections:
+           self._initiate_track(detections[detection_idx])
+       self.tracks = [t for t in self.tracks if not t.is_deleted()]
+
+       # Update distance metric.
+       active_targets = [t.track_id for t in self.tracks if t.is_confirmed()]
+       features, targets = [], []
+       for track in self.tracks:
+           if not track.is_confirmed():
+               continue
+           features += track.features
+           targets += [track.track_id for _ in track.features]
+           track.features = []
+       self.metric.partial_fit(
+           np.asarray(features), np.asarray(targets), active_targets)
+
+   def _match(self, detections):
+
+       def gated_metric(tracks, dets, track_indices, detection_indices):
+           features = np.array([dets[i].feature for i in detection_indices])
+           targets = np.array([tracks[i].track_id for i in track_indices])
+           cost_matrix = self.metric.distance(features, targets)
+           cost_matrix = linear_assignment.gate_cost_matrix(
+               self.kf, cost_matrix, tracks, dets, track_indices,
+               detection_indices)
+
+           return cost_matrix
+
+       # Split track set into confirmed and unconfirmed tracks.
+       confirmed_tracks = [
+           i for i, t in enumerate(self.tracks) if t.is_confirmed()]
+       unconfirmed_tracks = [
+           i for i, t in enumerate(self.tracks) if not t.is_confirmed()]
+
+       # Associate confirmed tracks using appearance features.
+       matches_a, unmatched_tracks_a, unmatched_detections = \
+           linear_assignment.matching_cascade(
+               gated_metric, self.metric.matching_threshold, self.max_age,
+               self.tracks, detections, confirmed_tracks)
+
+       # Associate remaining tracks together with unconfirmed tracks using IOU.
+       iou_track_candidates = unconfirmed_tracks + [
+           k for k in unmatched_tracks_a if
+           self.tracks[k].time_since_update == 1]
+       unmatched_tracks_a = [
+           k for k in unmatched_tracks_a if
+           self.tracks[k].time_since_update != 1]
+       matches_b, unmatched_tracks_b, unmatched_detections = \
+           linear_assignment.min_cost_matching(
+               iou_matching.iou_cost, self.max_iou_distance, self.tracks,
+               detections, iou_track_candidates, unmatched_detections)
+
+       matches = matches_a + matches_b
+       unmatched_tracks = list(set(unmatched_tracks_a + unmatched_tracks_b))
+       return matches, unmatched_tracks, unmatched_detections
+
+   def _initiate_track(self, detection):
+       mean, covariance = self.kf.initiate(detection.to_xyah())
+       class_name = detection.get_class()
+       self.tracks.append(Track(
+           mean, covariance, self._next_id, self.n_init, self.max_age,
+           detection.feature, class_name))
+       self._next_id += 1
+
+   def draw_box(self,frame):
+       """draw boudning box for detected persons. Display counts of unique people
+           and number of people moving left. Frame after adding text and box
+
+       Parameters
+       ----------
+       self
+       frame
+
+       Returns
+       ----------
+       Frame
+       """
+       disp_tol = 3
+       cmap = plt.get_cmap('tab20b')
+       colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+       self.current_count = 0
+       c_right=0 
+       c_left=0
+       for track in self.tracks:
+           
+           if not track.is_confirmed() or track.time_since_update > 1:
+               continue 
+           bbox = track.to_tlbr()
+           class_name = track.get_class()
+           
+           # Track center movement
+           center = (int(((bbox[0]) + (bbox[2]))/2), int(((bbox[1])+(bbox[3]))/2))
+           self.center[track.track_id].append(center)
+           
+           color = colors[int(track.track_id) % len(colors)]
+           color = [i * 255 for i in color]
+
+           lr =''
+           
+           self.cum_disp[track.track_id] = 0
+           disp = 0
+           
+           for j in range(1, len(self.center[track.track_id])):
+               if self.center[track.track_id][j-1] is None or self.center[track.track_id][j] is None:
+                   continue
+               thickness = int(np.sqrt(64/float(j+1))*2)
+               #cv2.line(frame, (self.center[track.track_id][j-1]), (self.center[track.track_id][j]), color, thickness)
+          
+           # Calculate center displacement
+               disp = self.center[track.track_id][j][0]-self.center[track.track_id][j-1][0]
+               self.cum_disp[track.track_id] += disp
+           
+           
+           
+           # draw bbox on screen
+           
+           cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+           cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(5)*8, int(bbox[1])), color, -1)
+           cv2.putText(frame, str(track.track_id)+lr,(int(bbox[0]), int(bbox[1]-10)),0, 0.5, (255,255,255),1)
+           
+           self.counter.append(int(track.track_id))
+           self.current_count += 1
+      
+              
+       # Count the number of tracks that have Total displacement > 0 or < 0
+       t_lefts = 0
+       t_rights = 0
+       for k in range(len(self.cum_disp)):
+           if self.cum_disp[k]<-disp_tol:
+             t_lefts +=1
+           elif self.cum_disp[k]>disp_tol:
+             t_rights += 1
+       print("Total Left Moves:",t_lefts)
+       print("Total Right Moves:",t_rights)
+
+       self.unique_count = len(set(self.counter))
+       cv2.putText(frame, "Current Persons: " + str(self.current_count), (1050, 50), 0, 0.5, (0, 0, 255), 1)
+       cv2.putText(frame, "Unique Persons: " + str(self.unique_count), (1050,80), 0, 0.5, (0,0,255), 1)
+       cv2.putText(frame, "Total Left Moving: " + str(t_lefts), (1050, 110), 0, 0.5, (0, 0, 255), 1)
+       
+       return frame
